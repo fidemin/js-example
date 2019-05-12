7b9746620f222bd7e6d5b797f952dabe8966fa6c
Yunhong Min <yhmin84@gmail.com>
2019-05-12 11:29:19 +0900

    generator with return

--- generator/generator.js
+++ generator/generator.js
@@ -15,3 +15,18 @@ console.log(it.next());
 console.log(it.next());
 console.log(it.next());
 console.log(it.next());
+
+console.log("it 2");
+function* ex_return() {
+  const end = yield 'red';
+  if (!!end) {
+    return ; // no return value for generator is recommended
+  }
+  yield 'blue'
+  yield 'green'
+}
+
+const it2 = ex_return();
+
+console.log(it2.next());
+console.log(it2.next(true));
